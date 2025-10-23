73987dffb7cca1ddf0a92efc61066977abb74bd6
typachortik <typachortik@gmail.com>
2025-10-23 16:12:48 +0300

    +fixes adminPage

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,27 +1,30 @@
 <template>
-  <div class="admin-page">
-    <h1>Админка</h1>
-    <form @submit.prevent="saveData" v-if="data">
-      <div>
-        <label>Position (число):</label>
-        <input v-model.number="data.position" type="number" />
-      </div>
-      <div>
-        <label>Percent:</label>
-        <input v-model.number="data.percent" type="number" step="0.1" />
-      </div>
-      <div>
-        <label>Year:</label>
-        <input v-model.number="data.year" type="number" />
-      </div>
-      <div>
-        <label>Days:</label>
-        <input v-model.number="data.days" type="number" />
-      </div>
-      <button type="submit">Сохранить</button>
-      <p v-if="message" :class="{ error: isError }">{{ message }}</p>
-    </form>
-    <p v-else>Загрузка...</p>
+  <div class="page">
+    <div class="admin-page">
+      <h1>Админка</h1>
+      <p>Изменение данных правого блока главной страницы, которая отрисовывает статистику</p>
+      <form @submit.prevent="saveData" v-if="data">
+        <div class="inputData">
+          <label>Position:</label>
+          <input v-model.number="data.position" type="number" />
+        </div>
+        <div class="inputData">
+          <label>Percent:</label>
+          <input v-model.number="data.percent" type="number" step="0.1" />
+        </div>
+        <div class="inputData">
+          <label>Year:</label>
+          <input v-model.number="data.year" type="number" />
+        </div>
+        <div class="inputData">
+          <label>Days:</label>
+          <input v-model.number="data.days" type="number" />
+        </div>
+        <button type="submit">Сохранить</button>
+        <p v-if="message" :class="{ error: isError }">{{ message }}</p>
+      </form>
+      <p v-else>Загрузка...</p>
+    </div>
   </div>
 </template>
 
@@ -34,7 +37,6 @@ const isError = ref(false)
 
 const API_URL = 'http://127.0.0.1:8000/api/data/'
 
-// Загрузка данных
 onMounted(async () => {
   try {
     const res = await fetch(API_URL)
@@ -49,7 +51,6 @@ onMounted(async () => {
   }
 })
 
-// Сохранение данных
 async function saveData() {
   try {
     const res = await fetch(API_URL, {
@@ -74,17 +75,51 @@ async function saveData() {
 </script>
 
 <style scoped>
+.inputData {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+.page {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  backdrop-filter: blur(5px);
+  min-height: 100vh;
+}
 .admin-page {
-  max-width: 600px;
-  margin: 2rem auto;
-  padding: 1rem;
+  padding: 20px;
+  background-color: #92929225;
+  display: flex;
+  margin: 150px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  width: 400px;
+  border-radius: 30px;
+  p {
+    text-align: center;
+  }
+}
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 5px;
 }
 input {
-  width: 100%;
-  padding: 0.5rem;
-  margin: 0.5rem 0 1rem;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+  width: 100px;
+  padding: 5px;
+  border: 5px solid white;
+  border-radius: 15px;
+  text-align: center;
+  transition: all 0.5s;
+}
+input:focus {
+  border: 5px solid orangered;
+  background-color: rgba(255, 68, 0, 0.041);
 }
 button {
   background: #42b883;
